24300442b0c375af5dc9f382cd265b805ef3cd40
huangwei37 <huangwei37@guazi.com>
2020-01-09 20:14:39 +0800

    merge dev 1.9

--- linkedlist/linkedlist.h
+++ linkedlist/linkedlist.h
@@ -1,13 +1,23 @@
-
 #ifndef LINKED_LIST
 
 #define LINKED_LIST 1
 
+enum lexical_token_type {
+  tag = 1,
+  text,
+};
+
 struct LNode {
   char* type;
+  char* key;
+  char* value;
   struct LNode* next;
 };
 
 typedef struct LNode L_Node;
+
+L_Node* append_lnode(L_Node* prev, L_Node* born);
+
+void test_lnode();
 
 #endif
